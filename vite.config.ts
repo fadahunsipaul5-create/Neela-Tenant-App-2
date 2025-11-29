4af4e579a186b87a1298e783697887149744c069
Fadahunsi-Paul <scandiclamar@gmail.com>
2025-11-29 03:35:08 +0000

    From-Pauls-methl.

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,12 @@ export default defineConfig(({ mode }) => {
       server: {
         port: 3000,
         host: '0.0.0.0',
+        proxy: {
+          '/media': {
+            target: process.env.VITE_API_URL || 'http://127.0.0.1:8000',
+            changeOrigin: true,
+          },
+        },
       },
       plugins: [react()],
       define: {
